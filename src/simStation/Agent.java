872f0a2b126d39c682a2e69134d846e52a7dc3bc
BobMak <vlmr.mko@gmail.com>
2021-04-09 18:36:52 -0700

    agent updates

--- src/simStation/Agent.java
+++ src/simStation/Agent.java
@@ -5,28 +5,33 @@ public class Agent implements Runnable {
     int y;
     String name;
     Heading heading;
+    AppState state;
     public Agent(int x, int y, String name, Heading heading) {
         this.x = x;
         this.y = y;
         this.name = name;
         this.heading = heading;
+        this.state = AppState.READY;
     }
 
     @Override
     public void run() {
-
     }
 
     public void start() {
+        this.state = AppState.RUNNING;
     }
 
     public void suspend() {
+        this.state = AppState.SUSPENDED;
     }
 
     public void resume() {
+        this.state = AppState.RUNNING;
     }
 
     public void stop() {
+        this.state = AppState.STOPPED;
     }
 
     public int getY() {
---
+++ src/simStation/SimPanel.java
@@ -0,0 +1,24 @@
+package simStation;
+import javax.swing.*;
+
+import mvc.*;
+
+
+public class SimPanel extends AppPanel {
+    private JButton change;
+    public SimPanel(AppFactory factory) {
+        super(factory);
+        change = new JButton("Change");
+        change.addActionListener(this);
+        controlPanel.add(change);
+        //add(buttonPanel);
+        //add(view);
+    }
+
+    public static void main(String[] args) {
+        AppFactory factory = new SimStationFactory();
+        AppPanel panel = new SimPanel(factory);
+        panel.display();
+    }
+
+}
