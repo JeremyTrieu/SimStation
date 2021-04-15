4b6ec7410b23ef404332abc7e34e4348708e57b3
JeremyTrieu <jeremytrieu1612@gmail.com>
2021-04-14 18:51:03 -0700

    Update RandomWalkSimulation.java
    randomWalk edited.

--- src/randomWalk/RandomWalkSimulation.java
+++ src/randomWalk/RandomWalkSimulation.java
@@ -13,6 +13,12 @@ class RandomWalkFactory extends SimulationFactory {
 }
 
 public class RandomWalkSimulation extends Simulation {
-  AppPanel panel = new SimulationPanel(new RandomFactory());
-  panel.display();
+  public void populate() {
+    for(int i = 0; i < 15; i++)
+      addAgent(new Drunk());
+  }
+  public static void main(String[] args) {
+    AppPanel panel = new SimulationPanel(new RandomFactory());
+    panel.display();
+  }
 }
