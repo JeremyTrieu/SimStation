08287802e168f0dd6b15f99ade881903acb89d06
Suiha <veronica.ss.hu@gmail.com>
2021-04-16 11:16:24 -0700

    implementation of prisoner's dilemma tournament using 4 different strategies

---
+++ src/pdt/EvilStrategy.java
@@ -0,0 +1,17 @@
+package pdt;
+
+public class EvilStrategy extends Strategy
+{
+    public EvilStrategy(Prisoner p)
+    {
+        super(p);
+    }
+
+    // This agent will never cooperate.
+    public boolean cooperate()
+    {
+        return false;
+    }
+
+    public String toString() { return "EvilStrategy"; }
+}
---
+++ src/pdt/PDTFactory.java
@@ -0,0 +1,20 @@
+package pdt;
+
+import mvc.*;
+import simStation.*;
+
+public class PDTFactory extends SimStationFactory
+{
+    public Model makeModel() { return new PDTournament(); }
+    public String getTitle() { return "Prisoner's Dilemma Tournament"; }
+
+    public Command makeEditCommand(Model model, String type)
+    {
+        return new PDTCommand(model, type);
+    }
+
+    public String about()
+    {
+        return "SimStation Customization, Prisoner's Dilemma by Team 8: Jeremy Trieu, Vlad Makarenko, Veronica Hu";
+    }
+}
