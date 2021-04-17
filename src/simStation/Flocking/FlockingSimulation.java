a14e4e8c7019e22a50855ebd9b08ac16d0b37629
JeremyTrieu <jeremytrieu1612@gmail.com>
2021-04-17 02:38:40 -0700

    Update FlockingSimulation.java
    FlockingSimulation updated.

--- src/simStation/Flocking/FlockingSimulation.java
+++ src/simStation/Flocking/FlockingSimulation.java
@@ -20,22 +20,24 @@ public class FlockingSimulation extends Simulation {
     }
     public String[] getStats() {
       HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
       map.put(1, 0);
-      map.put(2, 0);
-      map.put(3, 0);
-      map.put(4, 0);
-
-      Iterator hmIterator = map.entrySet().iterator();
       List<String> temp = new LinkedList<String>();
       for (int i = 0; i < this.getAgents.size(); i++) {
-          if (!this.getAgents.get(i).isStopped()) {
-            while (hmIterator.hasNext()) {
-              
-            }
+          if map.containsKey(this.getAgents.get(i).getSpeed()) {
+            map.get(i) += 1;
+          }
+          else {
+            map.put(this.getAgents.get(i).getSpeed(), 1);
           }
       }
-      String[] stats =
+      for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+        temp.add("#birds @ speed " + e.getKey() + " = " + e.getValue());
+      }
+      String[] stats = new String[temp.size()];
+      for (int i = 0; i < stats.length; i++) {
+        stats[i] = temp.get(i) + "\n";
+      }
+      return stats;
     }
   }
 }
