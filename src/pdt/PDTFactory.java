91e8c055f55b92f1e592b33c4f3046570d9f2a93
Suiha <veronica.ss.hu@gmail.com>
2021-04-18 13:04:36 -0700

    PDTCommand no longer needed

--- src/pdt/PDTFactory.java
+++ src/pdt/PDTFactory.java
@@ -9,11 +9,6 @@ public class PDTFactory extends SimStationFactory
     public View makeView(Model model) { return new PrisonerView(model); }
     public String getTitle() { return "Prisoner's Dilemma Tournament"; }
 
-    public Command makeEditCommand(Model model, String type)
-    {
-        return new PDTCommand(model, type);
-    }
-
     public String about()
     {
         return "SimStation Customization, Prisoner's Dilemma by Team 8: Jeremy Trieu, Vlad Makarenko, Veronica Hu";
